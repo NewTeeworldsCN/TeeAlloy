cc7f27e4dca9d417c41e8edf56edf64b76cbd523
Bamcane <Bamcane@teemidnight.online>
2025-09-28 19:53:25 +0800

    HTTPS

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ class Config:
     DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://teealloytest:test@localhost:5432/teealloydb")
     DB_MIN_CONN = int(os.environ.get("DB_MIN_CONN", 2))
     DB_MAX_CONN = int(os.environ.get("DB_MAX_CONN", 10))
+    SESSION_COOKIE_SECURE = True
+    REMEMBER_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Strict'
